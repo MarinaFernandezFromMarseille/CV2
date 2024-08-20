2a63bd215e7d23a006f00a4fac0f51440e4dcc09
MarinaFernandezFromMarseille <168725135+MarinaFernandezFromMarseille@users.noreply.github.com>
2024-08-20 13:40:52 +0200

    start right part

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
 </head>
 <body>
     <main>
+        <section class = "container">
+
+        <section class = "left">
         <img src="./ASSETS/Pictures/unnamed.jpg" alt="Profile Picture">
         <article>
             <h1>Mes infos</h1>
-            <p>07.52.05.65.62</p>
-            <p>marinafernandezproc@gmail.com</p>
-            <p>33 allée Léon Gambetta</p>
+            <p><img class="icon" src = "./ASSETS/Pictures/icons8-smartphone-24.png" alt = "smartphone Picture"> 07.52.05.65.62</p>
+            <p><img class="icon" src = "./ASSETS/Pictures/icons8-courrier-24.png" alt = "mail Picture"> marinafernandezproc@gmail.com</p>
+            <p><img class="icon" src = "./ASSETS/Pictures/icons8-position-24.png" alt = "adress Picture"> 33 allée Léon Gambetta</p>
             <p>13001 Marseille</p>
         </article>
 
@@ -35,6 +38,30 @@
                 <div class="skill-bar" id="jsSkill">js:10%</div>
             </div>
         </article>
+
+        <br>
+
+        <article>
+            <h1>Mes projets</h1>
+            <a href= "#">Projet1</a>
+            <a href = "#">Projet2</a>
+            <a href = "#">Projet3</a>
+        </article>
+        </section>
+
+        <section class = "right">
+            <article>
+            <h2>Marina Fernandez</h2>
+            <h3>Etudiante en Dev Web Mobile</h3>
+            </article>
+        <fieldset>
+            <legend><img class = "icon" src = "./ASSETS/Pictures/icons8-point-d'interrogation-24.png" alt = "home Picture">A propos de moi </legend>
+            <p>Je suis actuellement étudiante chez la Plateforme, en Dev Web Mobile. J'ai pour projet de continuer mon cursus le plus loin possible afin d'acquérir le plus de compétences possibles 
+                dans le but d'obtenir un niveau ingénieur et ainsi pouvoir par la suite travailler soit en entreprise, soit à mon compte. </p>
+            </fieldset>
+        </section>
+</section>
+
     </main>
     <footer>
     </footer>
